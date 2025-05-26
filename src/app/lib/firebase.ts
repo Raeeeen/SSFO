4cc3ab60d019cf7e9364f5c7008ac38c3e47db5b
Rafael Enrique Rodriguez <145899102+Raeeeen@users.noreply.github.com>
2025-05-26 14:20:44 +0800

    Update firebase.ts

--- src/app/lib/firebase.ts
+++ src/app/lib/firebase.ts
@@ -7,14 +7,7 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCBLQlyG5wMSuxXfep8AkkAQz73cuFD9B4",
-  authDomain: "dtrsystem-d3416.firebaseapp.com",
-  databaseURL: "https://dtrsystem-d3416-default-rtdb.firebaseio.com",
-  projectId: "dtrsystem-d3416",
-  storageBucket: "dtrsystem-d3416.firebasestorage.app",
-  messagingSenderId: "747066428637",
-  appId: "1:747066428637:web:4f3e0157facd5b98efdf34",
-  measurementId: "G-BV2ZHGEY5D"
+
 };
 
 // Initialize Firebase
